faf4f31a210e1a1fd0854fd71a21b85387f8861b
coldcoder126 <1348390090@qq.com>
2022-09-30 01:33:41 +0800

    2 lua scripts

--- pkg/cache/lua.go
+++ pkg/cache/lua.go
@@ -11,11 +11,30 @@ var Lua = make(map[int]string)
 
 const (
 	PointSub = iota
+	PostToSet
+	PostToQue
 )
 
 var scripts = map[int]string{
-	PointSub: "local subNum = tonumber(ARGV[1]) \nlocal curNum = tonumber(cache.call('hget',KEYS[1],KEYS[2])) \nif (subNum > curNum) \nthen \nreturn {-1} \nelse \ncache.call('hincrby',KEYS[1],KEYS[2],-subNum) \nreturn {1} end",
+	PointSub:  "local subNum = tonumber(ARGV[1]) \nlocal curNum = tonumber(cache.call('hget',KEYS[1],KEYS[2])) \nif (subNum > curNum) \nthen \nreturn {-1} \nelse \ncache.call('hincrby',KEYS[1],KEYS[2],-subNum) \nreturn {1} end",
+	PostToSet: "",
+	PostToQue: "",
 }
+
+//将帖子信息放入zset中
+//local val = ARGV[1]
+//local len = tonumber(ARGV[2])
+//redis.call('ZADD',KEYS[1],tonumber(os.time()),val)
+//redis.call('ZREMRANGEBYRANK',KEYS[1],len,-1)
+//redis.call('EXPIREAT',KEYS[1],tonumber(os.time()))
+
+//将帖子信息按时间顺序放入队列中
+//key = KEYS[1]
+//val = ARGV[1]
+//local len = tonumber(ARGV[2])
+//redis.call('LPUSH',key,val)
+//redis.call('LTRIM',key,0,len)
+//redis.call('EXPIREAT',KEYS[1],tonumber(os.time()))
 
 func InitLua(client *redis.Client) error {
 	// 清除Redis上所有脚本
